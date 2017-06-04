d2ec2b3a72588d4f1bf55c248fa739b72cc96b9f
planetacomputer <evapardeza123@gmail.com>
2017-06-04 12:33:09 +0200

    Fi de la part 3, amb nova ruta /1 per mostrar post. Doctrine entity

---
+++ src/Blogger/BlogBundle/Controller/BlogController.php
@@ -0,0 +1,43 @@
+<?php
+// src/Blogger/BlogBundle/Controller/BlogController.php
+
+namespace Blogger\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Blogger\BlogBundle\Entity\Blog;
+
+/**
+ * Controlador del Blog.
+ */
+class BlogController extends Controller
+{
+    /**
+     * Muestra una entrada del blog
+     */
+    public function showAction($id)
+    {
+        $em = $this->get('doctrine')->getManager();
+        /**
+        * Exemple d'insercio
+        
+        $blog1 = new Blog();
+        $blog1->setTitle('A day in paradise - A day with Symfony2');
+        $blog1->setBlog('Lorem ipsum dolor sit d us imperdiet justo scelerisque. Nulla consectetur...');
+        $blog1->setImage('beach.jpg');
+        $blog1->setAuthor('dsyph3r');
+        $blog1->setTags('symfony2, php, paradise, symblog');
+        $em->persist($blog1);
+        $em->flush();
+        **/
+
+        $blog = $em->getRepository('BloggerBlogBundle:Blog')->find($id);
+
+        if (!$blog) {
+            throw $this->createNotFoundException('Unable to find Blog post');
+        }
+
+        return $this->render('BloggerBlogBundle:Blog:show.html.twig', array(
+            'blog'      => $blog
+        ));
+    }
+}
